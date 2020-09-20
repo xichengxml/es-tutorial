8b1fabab6839adf53eeb3557289f11ad8ed0bfc2
xichengxml <xichengxml@163.com>
2020-09-20 23:39:55 +0800

    es

---
+++ elasticsearch/src/main/java/com/xicheng/elasticsearch/C02_GetData.java
@@ -0,0 +1,24 @@
+package com.xicheng.elasticsearch;
+
+import com.xicheng.elasticsearch.common.EsConfigUtil;
+import com.xicheng.elasticsearch.common.EsConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+
+/**
+ * description 根据id获取文档对象
+ *
+ * @author xichengxml
+ * @date 2020-09-20 21:37
+ */
+@Slf4j
+public class C02_GetData {
+
+    public static void main(String[] args) {
+        TransportClient client = EsConfigUtil.getClient();
+        GetResponse response = client.prepareGet(EsConstant.INDEX_NAME, EsConstant.TYPE_NAME, "830972").get();
+        String result = response.getSourceAsString();
+        log.info("C02_GetData main result: {}", result);
+    }
+}
